c51c0c5b9a8af40ac81f02036a9f7d3dff3e3f62
rafael <raffccc@gmail.com>
2015-03-24 09:03:12 -0300

    logpuzzle

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -11,7 +11,8 @@ import re
 import sys
 import urllib
 
-"""Logpuzzle exercise
+"""
+Logpuzzle exercise
 Given an apache logfile, find the puzzle urls and download the images.
 
 Here's what a puzzle url looks like:
@@ -20,42 +21,70 @@ Here's what a puzzle url looks like:
 
 
 def read_urls(filename):
-  """Returns a list of the puzzle urls from the given log file,
-  extracting the hostname from the filename itself.
-  Screens out duplicate urls and returns the urls sorted into
-  increasing order."""
-  # +++your code here+++
-  
+    """
+    Returns a list of the puzzle urls from the given log file,
+    extracting the hostname from the filename itself.
+    Screens out duplicate urls and returns the urls sorted into
+    increasing order.
+    """
+    
+    baseurl = 'http://www.' + filename[filename.find('_') + 1:]
+    
+    f = open(filename, 'rU')
+    
+    paths = re.findall('GET (\S+puzzle\S+) HTTP', f.read())
+    
+    uniquePaths = []
+    for path in paths:
+        if baseurl+path not in uniquePaths:
+            uniquePaths.append(baseurl+path)
+    
+    return sorted(uniquePaths)
 
 def download_images(img_urls, dest_dir):
-  """Given the urls already in the correct order, downloads
-  each image into the given directory.
-  Gives the images local filenames img0, img1, and so on.
-  Creates an index.html in the directory
-  with an img tag to show each local image file.
-  Creates the directory if necessary.
-  """
-  # +++your code here+++
-  
+    """
+    Given the urls already in the correct order, downloads
+    each image into the given directory.
+    Gives the images local filenames img0, img1, and so on.
+    Creates an index.html in the directory
+    with an img tag to show each local image file.
+    Creates the directory if necessary.
+    """
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+    
+    index = 0
+    imagePaths = []
+    for img_url in img_urls:
+        print 'Retrieving: ', img_url
+        imagePath = os.path.abspath(os.path.join(dest_dir, 'img' + index))
+        imagePaths.append(imagePath)
+        urllib.urlretrieve(img_url, imagePath)
+        index += 1
+        
+    f = open(os.path.abspath(os.path.join(dest_dir,'index.html'), 'w')
+    
+    f.write('<verbatim>\n <html> \n <body>' + ''.join(['<img src="' + imagePath + '"/>'  imagePath in imagePaths]) + '</body></html>')
+    
 
 def main():
-  args = sys.argv[1:]
+    args = sys.argv[1:]
 
-  if not args:
-    print 'usage: [--todir dir] logfile '
-    sys.exit(1)
+    if not args:
+        print 'usage: [--todir dir] logfile '
+        sys.exit(1)
 
-  todir = ''
-  if args[0] == '--todir':
-    todir = args[1]
-    del args[0:2]
+    todir = ''
+    if args[0] == '--todir':
+        todir = args[1]
+        del args[0:2]
 
-  img_urls = read_urls(args[0])
+    img_urls = read_urls(args[0])
 
-  if todir:
-    download_images(img_urls, todir)
-  else:
-    print '\n'.join(img_urls)
+    if todir:
+        download_images(img_urls, todir)
+    else:
+        print '\n'.join(img_urls)
 
 if __name__ == '__main__':
-  main()
+    main()
